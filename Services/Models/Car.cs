482648b5583b49ba57b86dc2efebc81d74335d49
mrchysen <mrchysen@mail.ru>
2024-08-06 12:39:48 +0700

    feat: add KeyAtt to Car model

--- Services/Models/Car.cs
+++ Services/Models/Car.cs
@@ -1,14 +1,24 @@
-﻿namespace Core.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.Models;
 
 public class Car
 {
+    [Key]
     public string Number { get; set; } = string.Empty;
+
     public bool IsWorked { get; set; } = false;
+
     public int FuelBegin { get; set; } = 0;
+
     public int FuelEnd { get; set; } = 0;
+
     public bool WasScreen { get; set; } = false;
+
     public bool Was24kmET { get; set; } = false;
+
     public List<int> Parking { get; set; } = new List<int>();
+
     public List<string> AddInformation { get; set; } = new List<string>();
 
     public override string ToString()
