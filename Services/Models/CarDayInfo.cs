3e9b00b011fa07a7ae4fbe4f58ac911be106bdff
mrchysen <mrchysen@mail.ru>
2024-08-06 13:20:37 +0700

    Add CarDayInfo class
    It contains info about cars in day + description

---
+++ Services/Models/CarDayInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.Models;
+
+public class CarDayInfo
+{
+    [Key]
+    public int Id { get; set; }
+    public DateTime Date { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public List<Car> Cars { get; set; } = new();
+}
