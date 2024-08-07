e0f698ae6936d562fdc52bc421e598a853b8e6cf
mrchysen <mrchysen@mail.ru>
2024-08-07 08:10:33 +0700

    feat: Add ICarDayInfoRepository and ICarDayInfoKeeper

---
+++ Services/Domains/Cars/Services/ICarDayInfoRepository.cs
@@ -0,0 +1,12 @@
+﻿using Core.Domains.Cars.Models;
+
+namespace Core.Domains.Cars.Services;
+
+public interface ICarDayInfoRepository
+{
+    CarDayInfo Get(int id);
+    IEnumerable<CarDayInfo> Get(DateTime date);
+    void Update(CarDayInfo carDayInfo);
+    void Delete(int id);
+    void Create(CarDayInfo carDayInfo);
+}
