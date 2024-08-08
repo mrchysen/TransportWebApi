33d0948aa414b5b55ce6336ff9b335f71a017db5
mrchysen <mrchysen@mail.ru>
2024-08-08 09:05:21 +0700

    feat: Add DbContextFactory to dbcontext class

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Core.Domains.Cars.Models;
 using Core.Domains.Cars.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 
 namespace Data;
 
@@ -29,5 +30,16 @@ public class ApplicationDbContext : DbContext, ICarDayInfoKeeper
     public void Create(CarDayInfo carDayInfo)
     {
         throw new NotImplementedException();
+    }
+}
+
+public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+        optionsBuilder.UseNpgsql("Host=localhost;Port=5438;Database=KTMdb;Username=postgres;Password=12345");
+
+        return new ApplicationDbContext(optionsBuilder.Options);
     }
 }
